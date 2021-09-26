93bb2317a2c402e2c251349f733dc2521504b08b
reyvver <reyvver.r@gmail.com>
2021-09-26 16:29:12 +0300

    Project is created

---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public GameObject Player;
+    public List<Transform> WayPoints; 
+    // Start is called before the first frame update
+
+    private NavMeshAgent playerAgent;
+    void Start()
+    {
+        playerAgent = Player.GetComponent<NavMeshAgent>();
+
+        playerAgent.SetDestination(WayPoints[0].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
